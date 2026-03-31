ad0509afa63c4df71e7c31ea394c66d335188c75
Stephengalo <escalans@go.stockton.edu>
2026-03-31 14:40:51 -0400

    Update Casale.sql
    Added Task Table

--- Casale.sql
+++ Casale.sql
@@ -30,6 +30,21 @@ CREATE TABLE PERMISSIONS (
 	CONSTRAINT Permission_PK PRIMARY KEY (permission_id)
 );
 
+CREATE TABLE TASKS (
+    task_id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(250) NULL,
+    status VARCHAR(20) NOT NULL DEFAULT 'Pending',
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    due_date DATE NULL,
+    CONSTRAINT Task_PK PRIMARY KEY (task_id),
+    CONSTRAINT Task_User_FK FOREIGN KEY (user_id) REFERENCES USERS(user_id),
+    CONSTRAINT Task_Status_Check CHECK (
+        status IN ('Pending', 'In Progress', 'Completed')
+    )
+);
+
 /**************
 INSERT STATEMENTS FOR PERMISSIONS
 kebab case, lowercase, <75 chars
